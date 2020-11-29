4add3daaf19542f220ca5ae1c34161d164470fac
salva <salvaruehling@gmail.com>
2020-11-29 02:28:11 +0000

    adjust package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='ENSO_GNN',
+    name='ProjectX_ENSO_GNN',
     version='',
     packages=['GNN_model1', "GNN_model2", "data_handling"],
     url='',
