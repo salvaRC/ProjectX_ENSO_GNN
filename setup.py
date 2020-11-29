dbbff676feacd0d3c75bb0b9914b70e30d0fa398
salva <salvaruehling@gmail.com>
2020-11-29 01:43:03 +0000

    add setup env instructions

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='ENSO_GNN',
     version='',
-    packages=['enso', 'GNN_exp1', "GNN_exp2", "data_handling"],
+    packages=['GNN_exp1', "GNN_exp2", "data_handling"],
     url='',
     license='',
     author='Salva Rühling Cachay',
